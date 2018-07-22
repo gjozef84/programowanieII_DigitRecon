e3b84a86d330cc90cc5f8a42c5e8daff26ad407c
Grzesiek Józefowicz <gjozef84@gmail.com>
2018-07-22 14:03:46 +0200

    Add method fileRead

--- src/main/java/Classifier.java
+++ src/main/java/Classifier.java
@@ -1,8 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by szypows_local on 22.07.2018.
  */
 public class Classifier {
     public static void main(String[] args) {
-        System.out.println("SIema cos dopisałem");
+
+        System.out.println(fileRead("trainingsample.csv"));
+
+    }
+
+
+    public static List<String> fileRead(String pathIn) {
+
+        List<String> list = new ArrayList<>();
+
+        try {
+            list = Files.readAllLines(Paths.get(pathIn));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
