40e1e8f8cf44d494f51a042ecd1e4be4e3acde5d
Stanisław Szypowski <sszypows@gmail.com>
2018-07-22 15:10:30 +0200

    Date as List<List<Integer>>

--- src/main/java/Classifier.java
+++ src/main/java/Classifier.java
@@ -1,25 +1,34 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * Created by szypows_local on 22.07.2018.
  */
 public class Classifier {
     public static void main(String[] args) {
-
-        System.out.println(fileRead("trainingsample.csv"));
-
+        List<String> list = fileRead("trainingsample.csv");
+        List<List<Integer>> columns = list.subList(1,list.size())
+                .stream()
+                .map(x->x.split(","))
+                .map(z->{
+                      return Arrays.stream(z)
+                              .map(y->Integer.parseInt(y))
+                              .collect(Collectors.toList());
+                })
+                .collect(Collectors.toList());
+        System.out.println(columns);
     }
 
 
     public static List<String> fileRead(String pathIn) {
 
         List<String> list = new ArrayList<>();
-
         try {
             list = Files.readAllLines(Paths.get(pathIn));
         } catch (IOException e) {
